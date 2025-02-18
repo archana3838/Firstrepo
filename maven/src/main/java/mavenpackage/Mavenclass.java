0fa29ada7ac02947387b9aeda24036d8bf73da82
archana <archana@QB-TP3-3075>
2025-02-18 14:38:46 +0530

    initial commit

---
+++ maven/src/main/java/mavenpackage/Mavenclass.java
@@ -0,0 +1,14 @@
+package mavenpackage;
+
+public class Mavenclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=2;
+		int b=3;
+		System.out.println(a+b);
+		
+	}
+
+}
